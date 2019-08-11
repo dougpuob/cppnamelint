f0f1c89ce14088fba96b3822d586a633a63e5faa
dougpuob <dougpuob@gmail.com>
2019-08-12 01:07:30 +0800

    Install essential tools.
    Why:
    Essential tools.
    
    HowToFix:
    1) sudo apt-get install python-3-pip
    2) sudo apt-get install cmake
    3) sudo apt-get install gcc-8
    4) sudo apt-get install g++-8

--- Script/BuildLinux.sh
+++ Script/BuildLinux.sh
@@ -21,7 +21,28 @@ git submodule update
 git submodule init
 git submodule update
 
+pip3 --version
+if [ "$?"!="0" ]; then    
+    sudo apt-get install python3-pip
+fi
+pip3 install docopt
 
+cmake --version
+if [ "$?"!="0" ]; then    
+    sudo apt-get install cmake
+fi
+
+${CC} --version
+if [ "$?"!="0" ]; then    
+    sudo apt-get install ${CC}
+fi
+
+${CXX} --version
+if [ "$?"!="0" ]; then    
+    sudo apt-get install ${CXX}
+fi
+
+# export NAMELINT_LLVM_LIB=~/my-data/my-cloud/@mydata/software/data/devtls/my-devtls/@Linux/3party-lib/ubuntu18.04.2-gcc8-x64/llvm
 python3 BuildFlow.py lint-format ../Source
 python3 BuildFlow.py proj-create .. ../Build ${BUILD_TYPE}
 
