e53f9132a2c17c0bdee8328b057b75020bd14d47
dougpuob <dougpuob@gmail.com>
2020-03-30 16:47:18 +0800

    Updating.

--- script/testcppnamelintlib-exec.py
+++ script/testcppnamelintlib-exec.py
@@ -12,25 +12,21 @@ from cppnamelintlib import File
 class TestCppNameLint_Exec(unittest.TestCase):
 
     def test_run(self):
-        
+
         exec_obj = Exec()
 
-        output_text:str = ''
         if platform.system() == 'Windows':
             error_code, output_text = exec_obj.run('whoami', ['/?'])
             self.assertEqual(True, 0 == error_code)
             self.assertEqual(True, '' != output_text)
-        # elif platform.platform() == 'Linux':
-        # elif platform.platform() == 'Darwin':
 
-        if platform.system() == 'Windows':
             found_index = output_text.find('Examples:')
             found_index = output_text.find('WHOAMI /USER /GROUPS /CLAIMS /PRIV')
             self.assertEqual(True, found_index >= 0)
         # elif platform.platform() == 'Linux':
         # elif platform.platform() == 'Darwin':
-        
-        
+
+
 
 if __name__ == '__main__':
     unittest.main()
