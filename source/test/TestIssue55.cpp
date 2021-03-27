248a7f1077ed45aaf6613ab2ac6ea349c102f9b8
Douglas Chen <dougpuob@gmail.com>
2021-03-27 13:28:35 +0800

    Add a test case for issue #55.
    This case copies from `https://github.com/changkun/modern-cpp-tutorial/blob/master/code/1/1.1.c.and.cpp`.

---
+++ source/test/TestIssue55.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <string>
+
+#include <gtest/gtest.h>
+
+#include "../Common.h"
+#include "../TraceMemo.h"
+
+
+// clang-format off
+namespace TestIssue {
+TEST(Issue55, FirstLineIsAnInvalidPathOnLinux) {
+  // https://github.com/dougpuob/cppnamelint/issues/55
+  const std::string SourceCode = "\
+    //                                                                            \n\
+    // 1.1.cpp                                                                    \n\
+    //                                                                            \n\
+    // chapter 1 introduction                                                     \n\
+    // modern cpp tutorial                                                        \n\
+    //                                                                            \n\
+    // created by changkun at changkun.de                                         \n\
+    // https://github.com/changkun/modern-cpp-tutorial                            \n\
+    //                                                                            \n\
+                                                                                  \n\
+    #include \"foo.h/\"                                                           \n\
+    #include <iostream>                                                           \n\
+    #include <functional>                                                         \n\
+                                                                                  \n\
+    int main() {                                                                  \n\
+        // use lambda expression                                                  \n\
+        [out = std::ref(std::cout << \"Result from C code: \" << add(1, 2))](){   \n\
+            out.get() << \".\n\";                                                 \n\
+        }();                                                                      \n\
+        return 0;                                                                 \n\
+    }";
+
+  AppCxt &AppCxt       = AppCxt::getInstance();
+  MemoBoard &MemoBoard = AppCxt.MemoBoard;
+  MemoBoard.Clear();
+  
+  EXPECT_EQ(true, 0 == RunCheckFormStream(MemoBoard, SourceCode));
+}
+
+}
+// clang-format on
